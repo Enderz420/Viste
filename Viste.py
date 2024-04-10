268620736146a79b97204cf899a48d17547a0143
Enderz420 <40892919+Enderz420@users.noreply.github.com>
2024-04-10 09:49:39 +0200

    Changed name

---
+++ Viste.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("Viste")
+root.configure(background="Pink")
+root.minsize(200,200)
+root.maxsize(500,500)
+root.geometry("300x300+50+50")
+root.mainloop()
