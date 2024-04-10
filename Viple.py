5040837c7089e2d82e6a6b1b3f90b8bd4e16c2f3
Enderz420 <40892919+Enderz420@users.noreply.github.com>
2024-04-10 09:48:22 +0200

    First commit, basic window with tkinter

---
+++ Viple.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("Viple")
+root.configure(background="Pink")
+root.minsize(200,200)
+root.maxsize(500,500)
+root.geometry("300x300+50+50")
+root.mainloop()
