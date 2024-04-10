fffb7ba97b3549343d786ba8bde1db11c8978e58
Enderz420 <40892919+Enderz420@users.noreply.github.com>
2024-04-10 10:08:27 +0200

    saved

--- Viste.py
+++ Viste.py
@@ -1,18 +1,1 @@
-from tkinter import *
-
-root = Tk()
-root.title("Viste")
-root.configure(background="")
-root.minsize(200,200)
-root.maxsize(1080,1080)
-root.geometry("300x300+50+50")
-
-# Create Label in our window
-text = Label(root, text="Nothing will work unless you do.")
-text.pack()
-text2 = Label(root, text="- Maya Angelou")
-text2.pack()
-
-left_frame = Frame(root, width="200", height="400")
-left_frame.grid(row=0, column=0, padx=10, pady=5)
-root.mainloop()
+import tkinter as tk
