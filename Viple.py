6cbd1ca6cc35a0962a49ad54be87da5739706110
Enderz420 <40892919+Enderz420@users.noreply.github.com>
2024-04-10 09:50:17 +0200

    Renamed

--- Viple.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-root = Tk()
-root.title("Viple")
-root.configure(background="Pink")
-root.minsize(200,200)
-root.maxsize(500,500)
-root.geometry("300x300+50+50")
-root.mainloop()
